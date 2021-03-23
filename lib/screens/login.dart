cb53194fcf8b0e2547618a043c23eeeec476a874
vikasdubeyyy <vikasdubeyyy@gmail.com>
2021-03-23 16:37:28 +0530

    Day 6 completed

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +26,7 @@ class Login extends StatelessWidget {
                 height: 20,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
               Padding(
@@ -29,6 +37,10 @@ class Login extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter Username", labelText: "Username"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -45,7 +57,40 @@ class Login extends StatelessWidget {
                             minimumSize: Size(130, 40)),
                         onPressed: () {
                           Navigator.pushNamed(context, MyRoutes.HOME);
-                        })
+                        }),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.HOME);
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 50 : 150,
+                        height: 40,
+                        // color: Colors.deepOrange,
+                        alignment: Alignment.center,
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text("Register",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18)),
+                        decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 20 : 6)),
+                      ),
+                    ),
                   ],
                 ),
               )
