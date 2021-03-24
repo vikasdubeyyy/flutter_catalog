5d674c36556abc9258496cb2cb0e5e66d533d5f4
vikasdubeyyy <vikasdubeyyy@gmail.com>
2021-03-24 15:32:41 +0530

    Day 13 - Load Local JSON File and Decode JSON

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/entities/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
-    int days = 30;
-    String name = "FlySpark";
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
