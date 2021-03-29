b5577d9259b96e223f0625c82d44c5504926c4e5
vikasdubeyyy <vikasdubeyyy@gmail.com>
2021-03-29 21:47:51 +0530

    Day 26 - State Management init using VelocityX

--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -4,24 +4,19 @@ import 'package:flutter_catalog/entities/cart.dart';
 import 'package:flutter_catalog/entities/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class AddToCart extends StatefulWidget {
+class AddToCart extends StatelessWidget {
   final Item catalog;
 
-  const AddToCart({
+  AddToCart({
     Key key,
     this.catalog,
   }) : super(key: key);
 
-  @override
-  _AddToCartState createState() => _AddToCartState();
-}
-
-class _AddToCartState extends State<AddToCart> {
   final _cart = CartModel();
 
   @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(widget.catalog) ?? false;
+    bool isInCart = _cart.items.contains(catalog) ?? false;
 
     return ElevatedButton(
         onPressed: () {
@@ -29,8 +24,8 @@ class _AddToCartState extends State<AddToCart> {
             isInCart = isInCart.toggle();
             final _catalog = CatalogModel();
             _cart.catalog = _catalog;
-            _cart.add(widget.catalog);
-            setState(() {});
+            _cart.add(catalog);
+            //setState(() {});
           }
         },
         style: ButtonStyle(
